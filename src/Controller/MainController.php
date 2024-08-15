b59a89c28984b6c401060b419a93ce3354328c6f
mraihannayuna <m.raihannayua13@gmail.com>
2024-08-15 22:10:10 +0700

    heuhehueeu

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
     }
 }
